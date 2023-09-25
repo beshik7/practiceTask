03f1ee38e06038cbdf023001dbcc4128fa2248f5
beshik7 <tommypdx7@gmail.com>
2023-09-25 06:52:05 -0700

    Practice task is done.

--- main/java/sky/pro/java/course5/task/Main.java
+++ main/java/sky/pro/java/course5/task/Main.java
@@ -1,5 +1,10 @@
 package sky.pro.java.course5.task;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Student student1 = new Student("John", 21);
+        Student student2 = new Student("John", 21);
+        System.out.println(student1.equals(student2));
+        System.out.println(student1.hashCode() == student2.hashCode());
+    }
 }
